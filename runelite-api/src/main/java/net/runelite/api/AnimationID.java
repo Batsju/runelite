be16f40b79c88e873d41ddbaacc571e797403bfc
Batsju <109969406+Batsju@users.noreply.github.com>
2022-08-12 01:41:22 +0200

    Ent trunk chopping
    Hello, i would love to see the chopping ent trunk animation added to the list. I dont see any reason why it is not already.
    Hope this will get added soon. 
    
    With kind regards,
    
    A happy user of your platform.

--- runelite-api/src/main/java/net/runelite/api/AnimationID.java
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -46,6 +46,7 @@ public final class AnimationID
 	public static final int WOODCUTTING_DRAGON_OR = 24;
 	public static final int WOODCUTTING_INFERNAL = 2117;
 	public static final int WOODCUTTING_3A_AXE = 7264;
+	public static final int WOODCUTTING_ENT_TRUNK = 2116
 	public static final int WOODCUTTING_CRYSTAL = 8324;
 	public static final int WOODCUTTING_TRAILBLAZER = 8778; // Same animation as Infernal axe (or)
 	public static final int CONSUMING = 829; // consuming consumables
